3f694f374c448693e433adbe1cce5814700dae61
José Francisco Meza Silva <jfranms06@gmail.com>
2026-02-18 00:29:20 +0000

    aaa

--- MetodosSim/Sim.java
+++ MetodosSim/Sim.java
@@ -2,6 +2,6 @@ public class Sim {
     public static void main (String args []) {
         System.out.println("Hello aaaa");
         System.out.println("Hola therian xd");
-        
+        //pruebaaaa
     }
 }
